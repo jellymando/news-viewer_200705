565b1133b96378ea8149f6bbf7724f16685188df
jellymando <cicikdi@naver.com>
2020-07-08 22:51:02 +0900

    NewsList, NewsItem Add

---
+++ src/components/NewsList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import NewsItem from './NewsItem';
+
+const sampleArticle = {
+    title: '제목',
+    description: '내용',
+    url: 'https://google.com',
+    urlToImage: 'https://via.placeholder.com/160'
+};
+
+const NewsList = () => {
+    return (
+        <div className="newsListBlock">
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+        </div>
+    );
+};
+
+export default NewsList;
